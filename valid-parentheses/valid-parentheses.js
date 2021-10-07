90ce71d84a3b634102bde7d0c9ed65fffa0f575a
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-10-07 15:33:20 -0700

    Time: 76 ms (69.58%), Space: 40.1 MB (31.45%) - LeetHub

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -3,40 +3,9 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // let hash = {
-    //     {: },
-    //     (: ),
-    //     [: ]
-    // }
-    
-//     let hash = {};
 
-//     for(let i = 0; i < s.length; i++) {
-        
-        
-//         if(s[i] === '{') {
-//             hash['}'] = '}';
-//             continue;
-//         }
-        
-//         if(s[i] === '(') {
-//             hash[')'] = ')';
-//             continue;
-//         }
-        
-//         if(s[i] === '[') {
-//             hash[']'] = ']';
-//             continue;
-//         }
-        
-//         if(hash[s[i]] !== undefined && i === s.length - 1) {
-//             return true;
-//         }
-        
-//         if(hash[s[i]] === undefined) {
-//             return false;
-//         }
     
+   
     if (s === null || s.length <= 0) return true;
     var cArr = s.split('');
     var stack = [];
@@ -46,12 +15,5 @@ var isValid = function(s) {
         else if (c === '(') stack.push(')');
         else if (stack.length === 0 || c !== stack.pop()) return false;
     }
-    if (stack.length === 0) return true;
-    return false;
-        
-        
-        
-        
-        
-    
+    return stack.length === 0;
 };
