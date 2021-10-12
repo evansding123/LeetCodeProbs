2e958e119934cf151729f72fef6bba284778ea98
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-10-11 18:21:40 -0700

    Time: 141 ms (24.26%), Space: 46.5 MB (30.89%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let hash = {};
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(hash[nums[i]] !== undefined) {
+            return true;
+        } 
+        
+        hash[nums[i]] = true;
+        
+        
+    }
+    
+    return false;
+};
