24c666897de679537969c92a0307a83ebc90f661
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-10-13 13:53:21 -0700

    Time: 150 ms (29.44%), Space: 40.4 MB (45.11%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,16 +4,21 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    
     let hash = {};
+    let result = [];
+    
+  
+    
     for(let i = 0; i < nums.length; i++) {
-        
         if(hash[target - nums[i]] !== undefined) {
             return [i, hash[target - nums[i]]];
         } else {
             hash[nums[i]] = i;
         }
-        
     }
     
-   
+    return result;
+    
+    
 };
