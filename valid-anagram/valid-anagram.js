57a4c6d90a410c787b61303bff8d9ee750ae6c92
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-10-14 14:47:31 -0700

    Time: 100 ms (60.26%), Space: 44.8 MB (18.27%) - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let first = s.split('').sort().join('');
+    let second = t.split('').sort().join('');
+    if(first === second) {
+        return true;
+    } else {
+        //console.log(first, second);
+        return false;
+    }
+};
