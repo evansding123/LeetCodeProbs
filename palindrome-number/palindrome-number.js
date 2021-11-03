08b49d9262f2ca9158343e59d11db5087b6ccda0
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-11-03 13:44:38 -0700

    Time: 184 ms (76.30%), Space: 48 MB (74.13%) - LeetHub

--- palindrome-number/palindrome-number.js
+++ palindrome-number/palindrome-number.js
@@ -3,14 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x <0) {
-        return false;
-    }
-    var string = x.toString().split("");
-    console.log('start: ', string.join(''), 'end', string.reverse().join(''));
-    if(string.join('') === string.reverse().join('')) {
+    let backward = x.toString().split('').reverse().join('');
+    if (x.toString() === backward) {
         return true;
-    } else {
-        return false;
     }
+    
+    return false;
 };
