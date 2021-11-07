e87777382249477abf85e48cb6cc1ae6eaeb95b7
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-11-06 19:25:47 -0700

    Time: 128 ms (33.79%), Space: 43.2 MB (67.30%) - LeetHub

--- move-zeroes/move-zeroes.js
+++ move-zeroes/move-zeroes.js
@@ -3,33 +3,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-//     let start = 0;
-//     let end = nums.length - 1;
-//     let count = 0;
+   
     
-//     for(let i = 0; i < nums.length; i++) {
-//         if(nums[i] === 0) {
-//             count++
-//         }
-//     }
-//     while(start < end) {
-//         if(nums[start] === 0) {
-//             let temp = nums[start + 1];
-//             nums[start] = temp;
-//             nums[start + 1] = 0;
-//         }
+    let j = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            let temp = nums[j]; 
+            nums[j] = nums[i];
+            nums[i] = temp;
+        } 
         
-//         start++;
-//     }
-    
-    
-    for(let i = nums.length - 1; i >= 0; i--) {
-        if(nums[i] === 0) {
-            nums.push(0);
-            nums.splice(i, 1)
+        if(nums[j] !== 0) {
+            j++;
         }
+        
+        
+        
+       
     }
     
+    
+   
     
     
     
