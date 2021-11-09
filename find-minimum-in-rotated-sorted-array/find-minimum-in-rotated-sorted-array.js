23cfcd90eefe9c94974bf380a472b27cdf13522a
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-11-09 15:48:45 -0800

    Time: 68 ms (92.21%), Space: 39.2 MB (20.73%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -2,39 +2,64 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-    //binary search
+var findMin = function(arr) {
+//     //binary search
     
-    if(nums.length <= 1) {
-        return nums[0];
+    if(arr.length <= 1) {
+        return arr[0];
     }
     
-    let right = nums.length - 1;
+//     let right = nums.length - 1;
+//     let left = 0;
+//     let mid = Math.min(right + left / 2);
+    
+//     if(nums[right] > nums[0]) {
+//         return nums[0];
+//     }
+    
+//     while(right >= left) {
+        
+//         if(nums[mid + 1] < nums[mid]) {
+//             return nums[mid + 1];
+//         }
+        
+//         if(nums[mid - 1] > nums[mid]) {
+//             return nums[mid];
+//         }
+        
+//         if(nums[mid] > nums[0]) {
+//             mid++;
+//         } else {
+//             mid--;
+//         }
+        
+        
+//     }
+    
     let left = 0;
-    let mid = Math.min(right + left / 2);
+    let right = arr.length - 1;
     
-    if(nums[right] > nums[0]) {
-        return nums[0];
+    if(arr.length === 1) {
+        return arr[0];
     }
     
-    while(right >= left) {
-        
-        if(nums[mid + 1] < nums[mid]) {
-            return nums[mid + 1];
+    while(left <= right) {
+        let mid = left + Math.trunc(right - left / 2);
+        if(arr[mid] < arr[mid - 1]) {
+            return arr[mid];
+        } else if (arr[mid] > arr[mid + 1]) {
+            return arr[mid + 1];
+        } else if(arr[mid]< arr[0]){
+            right = mid - 1;
+        } else if(arr[mid] > arr[0]){
+            left = mid + 1;
         }
         
-        if(nums[mid - 1] > nums[mid]) {
-            return nums[mid];
-        }
-        
-        if(nums[mid] > nums[0]) {
-            mid++;
-        } else {
-            mid--;
-        }
+        //console.log(left, right)
         
         
     }
+    return arr[0];
         
    
     
