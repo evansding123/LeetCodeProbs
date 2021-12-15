392014b0ab19b5bdefac0012fcd145afe3f7a063
Evans Ding <80489190+evansding123@users.noreply.github.com>
2021-12-14 22:10:56 -0800

    Time: 105 ms (27.43%), Space: 40.1 MB (93.75%) - LeetHub

--- reverse-integer/reverse-integer.js
+++ reverse-integer/reverse-integer.js
@@ -3,48 +3,26 @@
  * @return {number}
  */
 var reverse = function(x) {
-if(x < Math.pow(-2, 31) || x > Math.pow(2, 31) ) {
-    return 0;
-}
-     console.log(Math.pow(-2, 31));
-   
-    var string = x.toString();
-    if(x === 0) {
-        return 0;
-    }
     
-     if(x < 0) {
-        string = string.substring(1);
-     }
-    
-    // if(string.length >= 10) {
-    //     return 0;
-    // }
-    var result = '';
-    // if(string[string.length -1] === 0) {
-    //     string
-    // }
-    for(var i = string.length - 1; i >=0; i--) {
-       result += string[i];
-    }
-    
-    
+    let s = x.toString();
+    let result = '';
     
     if(x < 0) {
-        var answer = Number(result) * -1;
-        if(answer < Math.pow(-2, 31)) {
-            return 0;
-        } else {
-            return answer;
-        }
-    }    
-    
-    if(Number(result) > Math.pow(2, 31)){
-        return 0;
-    } else {
-        return Number(result);
+        result += '-';
+        let word = s.split('');
+        word.shift();
+        s = word.join('');
+    }
+    for(let i = s.length - 1; i >= 0; i--) {
+        result += s[i];
     }
     
+    if((Number(result) <= 2**31 && Number(result) >= -1 * 2**31) ) {
+        return result;
+    } else {
+        return 0;
+    }
     
-    
+    //console.log(result);
+    return result;
 };
