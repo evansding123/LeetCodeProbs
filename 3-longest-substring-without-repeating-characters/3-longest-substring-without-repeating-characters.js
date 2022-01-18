2f867e1af0bf212d65d9e4c7f1f3658a418d0486
Evans Ding <80489190+evansding123@users.noreply.github.com>
2022-01-18 13:29:10 -0800

    Time: 4023 ms (5.01%), Space: 90.9 MB (5.10%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let window = [];
+    let array = [];
+   
+    s = s.split('');
+   
+    const checker = (string) => {
+        let hash = {};
+        for(let i = 0; i < string.length; i++) {
+            if(hash[string[i]] === undefined) {
+                hash[string[i]] = true
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    for(let i = 0; i < s.length; i++) {
+        window.push(s[i]);
+       console.log(window)
+        if(checker(window.join(''))) {
+            array.push(window.join(''));
+        } else {
+            window.shift();
+    
+        }
+    }
+    
+    
+    
+    let max = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].length > max) {
+            max = array[i].length;
+        }
+    }
+    console.log(array)
+    return max;
+    
+};
