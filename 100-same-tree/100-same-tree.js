45409537a125db9de469de05d2305b582279c85b
Evans Ding <80489190+evansding123@users.noreply.github.com>
2022-03-21 16:16:15 -0700

    Time: 89 ms (41.61%), Space: 42.7 MB (10.22%) - LeetHub

---
+++ 100-same-tree/100-same-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    //have to check both trees at the same time
+    const dfs = (node, node2) => {
+        if(node === null && node2 !== null) {
+            return false;
+        }
+        if(node !== null && node2 === null) {
+            return false;
+        }
+      if(node === null && node2 === null) {
+          return true;
+      }
+        
+        
+      if(node.val !== node2.val) {
+          return false;
+      }
+        
+      
+        return dfs(node.left, node2.left) && dfs(node.right, node2.right)
+    }
+    
+    
+    return dfs(p, q);
+};
