42f244b033317af62c8a03d532d315ca3d1aa961
Evans Ding <80489190+evansding123@users.noreply.github.com>
2022-03-24 18:34:49 -0700

    Time: 68 ms (85.76%), Space: 44.3 MB (11.42%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    //base case is if the neighbors is undefined
+    let hash = {};
+    
+    const dfs = (root) => {
+        
+        if(root === null) {
+            return root;
+        } 
+        
+        if(hash[root.val] !== undefined) {
+            return hash[root.val];
+        }
+        
+        let newNode = new Node(root.val, []);
+        hash[root.val] = newNode;
+      
+       for(neighbor of root.neighbors) {
+           newNode.neighbors.push(dfs(neighbor));
+       }
+            
+        
+        return newNode;
+    }
+    
+    
+    return dfs(node);
+    //return newNode;
+};
