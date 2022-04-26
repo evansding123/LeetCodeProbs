0e3efa682e79c1fa1aae18d7edf99b8a946d46c3
Evans Ding <80489190+evansding123@users.noreply.github.com>
2022-04-26 15:39:00 -0700

    Time: 187 ms (20.46%), Space: 50.3 MB (28.28%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -3,20 +3,30 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    //check other intervals with first number less than second number
-    //brute force way is to just iterate through every single number  and match every number
-    intervals = intervals.sort((a,b) => {return a[0] - b[0]})
-    let result = [];
-    let hash = {};
+    //have to determine if the intervals are overlapping
+    //is the array sorted in any way? looks like its sorted from the value of the first and second tuple
     
-    for(let i = 0; i < intervals.length; i++) {
-       if(result.length === 0 || result[result.length - 1][1] < intervals[i][0]) {
-           result.push(intervals[i]);
-       } else {
-           result[result.length - 1][1] = Math.max(result[result.length - 1][1], intervals[i][1])
-       }
+    //can determine if they are overalapping  if the first index of the second tuple is greater than second index of the first
+    
+    
+    const result = [];
+    let i = 0;
+    
+    intervals.sort((a, b) => {
+        return ((a[0] - b[0]))
+    })
+    
+    //console.log(intervals)
+    
+    while(i < intervals.length) {
+        if(intervals[i + 1] !== undefined && intervals[i][1] >= intervals[i + 1][0]) {
+            const firstNum = intervals[i + 1][0] < intervals[i][0] ? intervals[i + 1][0] : intervals[i][0];
+            const secondNum = intervals[i + 1][1] > intervals[i][1] ? intervals[i + 1][1] : intervals[i][1];
+            intervals.splice(i, 2, [firstNum, secondNum])
+        } else {
+            i++;
+        }
     }
     
-    
-    return result
+    return intervals;
 };
