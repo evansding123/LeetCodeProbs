e95603a7b58086f2bcb43f8f046ff84852688609
Evans Ding <80489190+evansding123@users.noreply.github.com>
2022-07-10 22:01:33 -0700

    Time: 10 ms (83.78%), Space: 47.1 MB (88.23%) - LeetHub

---
+++ 251-flatten-2d-vector/251-flatten-2d-vector.java
@@ -0,0 +1,36 @@
+import java.util.NoSuchElementException;
+
+class Vector2D {
+
+    // Constructor will put all the nums into this list.
+    private List<Integer> nums = new ArrayList<>();
+    // Keep track of where the Iterator is up to.
+    private int position = 0;
+
+    public Vector2D(int[][] v) {
+        // We need to iterate over the 2D vector, getting all the integers
+        // out of it and putting them into nums (a field).
+        for (int[] innerVector : v) {
+            for (int num : innerVector) {
+                nums.add(num);
+            }
+        }
+    }
+
+    public int next() {
+        // In Java, we throw a NoSuchElementException when next() is called
+        // on an exhausted Iterator.
+        if (!hasNext()) throw new NoSuchElementException();
+        // Store the number we need to return, as we still need to move position forward.
+        int result = nums.get(position);
+        // Move the position pointer forward by 1, so that it's ready for
+        // the next call to next, and gives a correct hasNext result.
+        position++;
+        return result;
+    }
+
+    public boolean hasNext() {
+        // There's nums left as long as position is a valid index of the list.
+        return position < nums.size();
+    }
+}
